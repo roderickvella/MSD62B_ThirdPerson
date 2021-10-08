ff8182352657b9b886ff8c5d59fd3b22449cb306
roderickvella <55999524+roderickvella@users.noreply.github.com>
2021-10-08 15:13:23 +0200

    added the canvas and scriptable object

---
+++ MSD62B_ThirdPerson/Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="GameItem",menuName ="Game Items abc/Create Game item",order =1)]
+public class ItemScriptableObject : ScriptableObject
+{
+    public string title;
+    public Sprite icon;
+    public int increaseValue;
+    public Type type;
+
+    public enum Type
+    {
+        Health,
+        Mana
+    }
+
+}
